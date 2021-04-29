1aff50ae7c3007f6329a5014ca95f9e07a3c4c62
Grovhub <Grovhub@users.noreply.github.com>
2021-04-29 07:26:36 +0200

    Project commited to git

---
+++ Assets/Movement.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    private float turner;
+    private float looker;
+    public float sensitivity;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        // is the controller on the ground?
+        if (controller.isGrounded)
+        {
+            //Feed moveDirection with input.
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            //Multiply it by speed.
+            moveDirection *= speed;
+            //Jumping
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+        }
+        turner = Input.GetAxis("Mouse X") * sensitivity;
+        looker = -Input.GetAxis("Mouse Y") * sensitivity;
+        if (turner != 0)
+        {
+            //Code for action on mouse moving right
+            transform.eulerAngles += new Vector3(0, turner, 0);
+        }
+        if (looker != 0)
+        {
+            //Code for action on mouse moving right
+            transform.eulerAngles += new Vector3(looker, 0, 0);
+        }
+        //Applying gravity to the controller
+        moveDirection.y -= gravity * Time.deltaTime;
+        //Making the character move
+        controller.Move(moveDirection * Time.deltaTime);
+
+
+
+    }
+
+    public float lookSpeed = 3;
+    private Vector2 rotation = Vector2.zero;
+    public void Look() // Look rotation (UP down is Camera) (Left right is Transform rotation)
+    {
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+        rotation.x = Mathf.Clamp(rotation.x, -15f, 15f);
+        transform.eulerAngles = new Vector2(0, rotation.y) * lookSpeed;
+        Camera.main.transform.localRotation = Quaternion.Euler(rotation.x * lookSpeed, 0, 0);
+    }
+
+}
